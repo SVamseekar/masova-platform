e830297c56dd1b8ce2b1b16ae5e3adf2d5d5ac9b
Soura <martisoura@gmail.com>
2026-05-17 21:56:44 +0530

    fix(payment): add com.MaSoVa.shared.messaging to component scan so MaSoVaRabbitMQConfig exchanges are declared

--- payment-service/src/main/java/com/MaSoVa/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/MaSoVa/payment/PaymentServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@SpringBootApplication(scanBasePackages = {"com.MaSoVa.payment", "com.MaSoVa.shared.security"})
+@SpringBootApplication(scanBasePackages = {"com.MaSoVa.payment", "com.MaSoVa.shared.security", "com.MaSoVa.shared.messaging"})
 @EnableMongoAuditing
 public class PaymentServiceApplication {
 
